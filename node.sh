f7729511e168d78709543fbf1f44bbb253394c8e
fastflyk <108046308+fastflyk@users.noreply.github.com>
2023-08-22 13:00:27 +0800

    Update node.sh

--- node.sh
+++ node.sh
@@ -3,5 +3,5 @@
 curl -o ~/node.py -L https://raw.githubusercontent.com/fastflyk/script/main/node.py &&
 cd ~ &&
 python3 -c 'from node import init; init()' &&
-python3 -c 'from node import yh; yh()' &&
+python3 -c 'from node import yh; yh()' && sysctl -p &&
 python3 node.py $1 $2 $3 $4
